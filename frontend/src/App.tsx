4417d6026f2243c480cb401a39fd17e763f96289
boumazian <email@domain.com>
2025-10-29 14:44:23 +0100

    only unauthenticated users can enter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
-import { Routes, Route } from "react-router"
+import { Route, Routes } from "react-router"
 import Auth from "./pages/Auth/Auth"
-import Chat from "./pages/Chat/page"
+import Chat from "./pages/Chat/Chat"
 
 import { Toaster } from "sonner"
+import { GuestRoute, PrivateRoute } from "./pages/PageGuards"
 
 const App: React.FC = () => {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/auth" element={<Auth />} />
-    </Routes>
-    <Toaster richColors position="top-right"/>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Chat />}/>
+        </Route>
+        <Route element={<GuestRoute />}>
+          <Route path="/auth" element={<Auth />}/>
+        </Route>
+      </Routes>
+      <Toaster richColors position="top-right"/>
     </>
   )
 }
--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -2,7 +2,7 @@
 const Chat: React.FC = () => {
   return (
     <>
-       <p className="text-3xl text-red-500">This is Chat page</p>
+       This is Chat page
     </>
   )
 }
